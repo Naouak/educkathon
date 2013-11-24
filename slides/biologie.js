c35f434f082ab221c9b2244b0cf75ed5f8a6d5e1
Naouak <tardiel@gmail.com>
2013-11-24 12:23:57 +0100

    Test content Added

---
+++ slides/biologie.js
@@ -0,0 +1,100 @@
+
+
+register_slides([
+
+	{ 
+		innerHTML: "<h1>Chapitre 6 :</h1><h2>La recherche de lien de parentés parmi les Vertébrés</h2><p>Plan :</p><ul><li>I. Les caractéristiques communes des êtres vivants</li><ul><li>1. Les caractéristiques communes des êtres vivants</li><li>2. Mise en application et exemples</li></ul><li>II. Établir des liens de parenté</li><ul><li>1. Caractère homologue</li><li>2. Caractère analogue</li><li>3. État ancestral et état dérivés</li><li>4. L'arbre de représentation des parentés</li><li>5. Exemple de la comparaison moléculaire</li></ul><li>III. La lignée Humaine</li></ul>",
+		
+		questions: []
+	},
+	
+	{ 
+		innerHTML: "<h3>I. Les caractéristiques communes des êtres vivants</h3><ul><li>1. Les caractéristiques communes des êtres vivants</li><li>2. Mise en application et exemples</li></ul>",
+		
+		questions: [
+			{
+				type: "qcm",
+				question: "Qu'est-ce qui rassemble l'ensemble des êtres vivants ?<br>Laquelle de ces caractéristiques est commune aux poissons, aux chimpanzés et à la baleine ?",
+				answers: [ "Lieu de vie", "Leur information génétique est constituée d'ADN", "Ils appartiennent tous à la famille des mammifères" ],
+				correct_answer_index: 2
+			}
+		]
+	},
+
+	{ 
+		innerHTML: "<h3>II. Établir des liens de parenté</h3><h4>1. Caractère homologue</h4><p>Caractère : <em>élément observable d'une organisation</em></p><p>Caractère homologue entre deux espèces s'il a été hérité d'un ancêtre commun et répond aux trois critères suivants :<ul><li>i. Plan d'organisation similaire<li></li>ii. Même position dans le plan d'organisation général<li></li>iii. Même origine embryonnaire</li></ul>",
+		
+		questions: [
+			{
+				type: "qcm",
+				question: "Quels sont les trois critères pour définir un caractère homologue ?",
+				answers: [ "Une même fonction du caractère", "Un ancêtre commun", "Le résultat d'une mutation", "Un plan d'organisation similaire", "Une position dans le plan d'organisation général de l'organisme différent, une autre origine embryologique", "Plan d'organisation similaire" ],
+				correct_answer_index: 4				
+			},
+			{
+				type: "qcm",
+				question: "<img src='img/slide3_question2.png' /><br>Partant du principe que ces deux espèces ont un ancêtre commun, parmi les caractères 1, 2 et 3 lesquels sont homologues ?",
+				answers: [ "1 - 2 - 3", "1 - 2", "2 - 3", "1 - 3", "1", "2", "3", "Aucune"  ],
+				correct_answer_index: 1		
+			},
+			{
+				type: "qcm",
+				question: "<img src='img/slide3_question3.png' /><br>Quel est l'intrus parmi ces trois organes ?",
+				answers: [ "aile de papillon", "aile de chauve-souris", "bras humain" ],
+				correct_answer_index: 1		
+			}
+		]
+	},
+	
+	{
+		innerHTML: "<h3>II. Établir des liens de parenté</h3><p>Mise en application</p><p><img src='img/slide4_application.png' /></p>",
+			
+		questions: []
+	},
+	{
+		innerHTML: "<h3>II. Établir des liens de parenté</h3><h4>2. Caractère analogue</h4><p>Deux caractères sont <strong>analogues</strong> :</p><ul><li>lorsqu'ils assurent des <strong>fonctions similaires</strong></li><li>qu'elles <strong>ne sont pas héritées d'un ancêtre commun</strong></li></ul>",
+			
+		questions: [
+			{
+				type: "qcm",
+				question: "Deux caractères sont analogues lorsque :",
+				answers: [ "Ils assurent une fonction différente et dérivent de l'évolution d'un ancêtre commun", "Ils ont une fonction similaire mais qui ne dérivent pas de l'évolution d'un ancêtre commun", "Ils assurent une fonction différente" ],
+				correct_answer_index: 1				
+			}
+		]
+	},
+	{
+		innerHTML: "<h3>II. Établir des liens de parenté</h3><p>Mise en application</p><p><img src='img/slide6_application.png' /></p>",
+			
+		questions: [
+			{
+				type: "open",
+				question: "<img src='img/slide6_question1.png' /><p>Expliquer en quoi ces deux ailes sont des caractères analogues entre la chauve-souris et le papillon.<br>Une réponse rédigée est attendue.</p>",
+				answers: [],
+				correct_answer_index: 0				
+			}
+		]
+	},
+	{
+		innerHTML: "<h3>II. Établir des liens de parenté</h3><h4>3. État ancestral et état dérivés</h4>",
+			
+		questions: []
+	},
+	{
+		innerHTML: "<h3>II. Établir des liens de parenté</h3><h4>4. L'arbre de représentation des parentés</h4>",
+			
+		questions: []
+	},
+	{
+		innerHTML: "<h3>II. Établir des liens de parenté</h3><h4>5. Exemple de la comparaison moléculaire</h4>",
+			
+		questions: []
+	},
+	{
+		innerHTML: "<h3>III. La lignée Humaine</h3>",
+			
+		questions: []
+	}
+	
+]);
+
---
+++ slides/health.js
@@ -0,0 +1,57 @@
+
+
+register_slides([
+
+	{ 
+		innerHTML: "<img src='soleil.jpg'><br>Determinant, <br> Element qui influence la santé d'un individu. <br> Effet positif ou negative <br> + determinant de santé <br> - facteur de risque <br>",
+		
+		questions: [
+			{
+				type: "qcm",
+				question: "Quel est le cheval blanc d'henry 4 ?",
+				answers: [
+					"blanc",
+					"gris",
+					"noir"
+				]
+			},
+			{
+				type: "qcm",
+				question: "Comment ça va ?",
+				answers: [
+					"bien",
+					"bof",
+					"je suis fatigué"
+				]
+			}
+		]
+	},
+
+	{
+		innerHTML: "Mise en application, <br> La qualité de l'air est un déterminant de santé <br> La pollution atmospherique est un facteur de risque",
+			
+		questions: [
+			{
+				type: "qcm",
+				question: "Le sport etant un determinant de santé, lequel de ces choix correspond au facteur de risque associé ?",
+				answers: [ "La pratique de l'atletisme", "Le sport", "L'absence d'activité physique" ],
+				correct_answer_index: 2
+			},
+			{
+				type: "qcm",
+				question: "Le determinant a un effet ……………  sur la santé.",
+				answers: [ "positive", "négative" ],
+				correct_answer_index: 0		
+			},
+			{
+				type: "qcm",
+				question: "Le facteur de risque a un effet ……………  sur la santé.",
+				answers: [ "positive", "négative" ],
+				correct_answer_index: 1		
+			}	
+
+		]
+	}
+	
+]);
+
